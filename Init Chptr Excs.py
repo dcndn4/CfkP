2cd1479eb6872074f537b7e09646c877b360c4b5
dcndn4 <stokes@iphouse.com>
2021-09-19 07:43:51 -0500

    initial exercises

---
+++ Init Chptr Excs.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Sep 19 07:24:10 2021
+
+@author: CS_Knit_tinK_SC
+"""
+print("hi python")
+print("hi Claire")
+# print("I should not be printed!")
+# pirnt("yesterday upon the stair")
+print("yesterday upon the stair")
+# print("you are a silly shoe")
+
+print("I saw a man who wasn't there")
+
+# this code prints out text to the shell
+print("text out to the shell")
+
+print("he wasn't there again today")
+print("I'm so happy to be learning how to code in python")
+#print('I wish to heck he'd go away')
+print("I wish to heck he'd go away")
+#print('I'm so happy to be learning how to code in python')
+print('I\'m so happy to be learning how to code in python')
+print("\"Kumusta\" is \"Hello\" in Tagalog!")
+print("Here is\n a sentence \n on many \n different lines")
+author = "Adrienne"     
