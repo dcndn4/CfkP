07ad9583e432f3e8b796f2376d023c886dbb2d9d
dcndn4 <stokes@iphouse.com>
2021-09-19 08:07:33 -0500

    Add exercise

--- Init Chptr Excs.py
+++ Init Chptr Excs.py
@@ -24,4 +24,53 @@ print("I wish to heck he'd go away")
 print('I\'m so happy to be learning how to code in python')
 print("\"Kumusta\" is \"Hello\" in Tagalog!")
 print("Here is\n a sentence \n on many \n different lines")
-author = "Adrienne"     
+# %%
+author = "Adrienne"
+print("Adrienne! Yo, Adrienne!!")
+# couldn't resist
+# %%
+reader = "Julie"
+#print(f"Author is" (author) "and reader might or might not be" (reader))
+# %%
+print(reader)
+print(author)
+# %% 
+reader = "Everyone_on_earth"
+print(reader)
+# %%
+unfavorite_number = 6
+type(unfavorite_number)
+# this didn't reply with the <class 'int'> text that book said it would, because am not in shell I think?
+
+#%%
+
+print("camelCase not best format!")
+#%%
+
+food = "tater_tots"
+f"I like {food}"
+print(f"I like {food}")
+
+print(f"books should be read by {reader}")
+
+poem = """
+    Yesterday upon the stair
+    I saw a man who wasn\'t there
+    he wasn\'t there again today
+    I wish to heck he\'d go away!
+    """
+
+print(f"{poem}")
+
+
+
+
+
+
+
+
+
+
+
+
+
