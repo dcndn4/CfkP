e3ac594da89de514df6d3f3fde453563b2179c01
dcndn4 <stokes@iphouse.com>
2021-11-07 19:10:55 -0600

    all more familiar

--- midway review.py
+++ midway review.py
@@ -232,6 +232,38 @@ for i in range(len(nachos_friends)):
 
 #%%
 
+
 has_zero_legs = 0
 has_two_legs = 0
 has_four_legs = 0
+#%%
+
+no_id_animals = [4, 0, 2, 4, 2, 0, 2, 4, 4, 2, 0, 2, 4]
+
+
+#%%
+for i in range(len(no_id_animals)):
+    if no_id_animals[i] == 0:
+        has_zero_legs += 1
+    if no_id_animals[i] == 2:
+        has_two_legs += 1
+    if no_id_animals[i] == 4:
+        has_four_legs += 1
+        
+# This works:      
+print(f'Animals with no legs: {has_zero_legs}')
+print(f'Animals with two legs: {has_two_legs}')
+print(f'Animals with four legs: {has_four_legs}')
+
+#%%
+
+# but this is even better!
+animal_summary = f'''       
+Animals with no legs: {has_zero_legs}
+Animals with two legs: {has_two_legs}
+Animals with four legs: {has_four_legs}
+'''
+
+print(animal_summary)
+
+
